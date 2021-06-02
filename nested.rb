989f41d598151bf91b8b3b877b91906311b429c6
Ian King <kickthechair@gmail.com>
2021-06-02 18:16:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+  programmer_hash[:grace_hopper]
 
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,7 +39,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
+     
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,7 +61,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages][0]
+     
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -80,8 +90,10 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
+programmer_hash[:yukihiro_matsumoto][:known_for] = {:languages => ["LISP", "C"]}
 end
+
 
 def changing_alan
 	programmer_hash = 
